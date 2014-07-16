c4d3b9ea90021ccb171ebafc54080af3076d27c4
favrin <favrin@gmail.com>
2014-07-16 09:31:50 +0100

    new with improved comments

--- README.txt
+++ README.txt
@@ -16,28 +16,52 @@
 
 
 Welcome to the PetriNet Simulator.
-This scripts allow you to simulate the behaviour of your Petri Network and to perform a simple analysis of the simulation results
+These scripts allow you to simulate the behaviour of your Petri Network and to perform a simple analysis of the simulation results
 
 Licenses	= in the "Licenses" folder you may find:
 		  
 		1) a copy of the "GNU General Public License"
 		2) a copy of the "GNU Lesser General Public License"
 
-Input format	= json file (merge_matrices.txt) obtained from the website "http://www.esyn.org"
-		  that file must be placed in the same folder of the Simulator
 
-Simulation script
-		simply run the simulator script
-		(if you've placed your input files in another folder, change the input path inside the script before running it)
-		for each iteration of the simulation, a file is created in the "Results" folder
+SIMULATION SCRIPT
+		NOTE: it requires the "rjson" package (Alex Couture-Beil, 6/2014), available at "http://cran.r-project.org/web/packages/rjson/index.html"
+		This script simulates the behaviour of your Petri Network; it implements Petri Net rules and the Gillespie algorithm.
+		The whole simulation is repeated for 100 times (users can change this "iterNumber" in the script).
 
-Analysis script
-		you can run it anytime, but you need to have some files in the "Results" folder to work with		
-		(if you've placed your input files in another folder, change the input path inside the script before running it)		
+		Simulation is repeated for 3000 steps (users can change this "stepsNumber" in the script); at each step:
+			first, Petri Net rules are employed to identify which transitions of the network are enabled to fire at each simulation step
+			second, the Gillespie algorithm is employed to choose which enabled transition actually fires at each simulation step
+				1) rates are calculated for each transition; they are function of the k parameter and the number of tokens in the pre-places (of the selected transition)
+OLD VERSION			2) a "waiting time" is calculated for each transition combining the correspondent rate and a randomly generated number
+					vectorProb  <- (-1/vectorProb)*log(runif(ktransitN,min=0, max=1))
+NEW VERSION			2) a "waiting time" is calculated for each transition by generating a random number from an exponential distribution, whose rate is the transition rate
+					vectorTime[x] <-rexp(1,vectorProb[x])  
+				3) the transition with the shortest waiting time is the transition that actually fires
+			then, rules are employed to move tokens among places according to the firing transition; the simulation time is increased by adding the waiting time
+		In this script, all the transitions can fire at the beginning of each step; if a disabled transition is chosen, the simulation is performed but the changes are not applied; 
+			the simulation is repeated by choosing among the remaining transitions; if there's no transitions left, the system has reached a dead state and the simulation stops
 
-		for each simulation file in the folder, values are interpolated at specific time points (according to the number of points you want to sample)
-		a time series is created by calculating the mean values at each time point among all the simulation
+		the input file is a json file (merge_matrices.txt) obtained from the website "http://www.esyn.org"
+			that file should be placed in the same folder of the Simulation script
+			(if you've placed your input files in another folder, change the input path inside the script before running it)
+		the output file is a matrix showing the amount of tokens in each place and the elapsed time at each simulation step
+			a matrix is created at each iteration run; 
+			all the matrices are placed inside the "Results" folder, which is located in the same folder of the input file
 
-		output: in the R interface - mean values of selected places, and a graph of the abundance of those places (or all the places) at different times
-			in the "finalmarkings.txt" file (values at the last time point for each simulation)
-			in the "timeseries.txt" file (mean values of all the simulation at each time point)
+ANALYSIS SCRIPT
+		NOTE: it requires the "zoo" package (Zeileis et al., 2/2014), available at "http://cran.r-project.org/web/packages/zoo/index.html"
+		it is a script to perform a simple analysis of the simulation results; therefore, it can be run anytime, but not before the simulation script
+			for each simulation file in the folder, values are interpolated at specific time points (according to the number of points you want to sample)
+				the number of points ("spotsN") can be changed in the script
+			outliers (too short or too long simulations) are identified and discarded for the following steps; 
+				the threshold (i.e. distance from the mean value) is expressed as numbers of standard deviations
+				this value ("sensSd") can be changed in the script
+			a mean time series is created by calculating the mean values at each time point of all the simulations.
+
+		the input files are the matrices located in the "Results" folder
+			those files should be already located in the same folder of the Analysis script
+			(if you've placed your initial json file in another folder, change the input path inside the script before running it)
+		output: in the R interface 		- mean values of selected places, and a graph of the abundance of those places (or all the places) at different times
+			in the "finalmarkings.txt" file - values at the last time point for each simulation
+			in the "timeseries.txt" file 	- mean values of all the simulation at each time point
--- Simulator - simulation.R
+++ Simulator - simulation.R
@@ -19,12 +19,12 @@
 ####################################
 
 
-library('rjson')
-stepsNumber	<- 3000       # Number of steps of each simulation (not the duration of the simulation itself)
-iterNumber	<- 100        # Number of times each simulation is repeated; all the results will be saved in separate files
+library('rjson')            # rjson package is loaded; it must be installed before running the script
+stepsNumber   <- 3000       # Number of steps of each simulation (not the duration of the simulation itself)
+iterNumber	  <- 100        # Number of times each simulation is repeated; all the results will be saved in separate files
 
-inputPath	    <- ""
-outputPath	  <- "Results/"
+inputPath	    <- ""         # by default, the input file will be read from the working directory
+outputPath	  <- "Results/" # by default, output files will be placed in this folder inside the working directory
 inputFileName	<- "merge_matrices.txt"
 inputFile	<- paste (inputPath, inputFileName, sep = "")
 inputData	<- fromJSON(paste(readLines(inputFile), collapse=""))
@@ -34,11 +34,12 @@ transitNames	<- inputData$tnames			  # Name of transitions
 placesNames	<- inputData$pnames			    # Name of places
 kplaceN 	<- length(placesNames)		   	# Number of places
 ktransitN 	<- length(transitNames) 		# Number of Transitions
-vectorPar   <- inputData$k				      # Mass Action parameters
-matrixInhibit  	<- do.call(rbind, inputData$inhib)	# matrix of the weights of inhibitory arcs, always going FROM places TO transitions
-inhibIndex 	<- which(matrixInhibit>=1, arr.ind=TRUE)#see where are inhibitions in the matrix
-matrixInward 	<- do.call(rbind, inputData$post)
-matrixOutward 	<- -1*(do.call(rbind, inputData$pre))
+#vectorPar	<- rep (1, ktransitN)			  # Mass Action parameters
+vectorPar 	<- inputData$k				      # Mass Action parameters
+matrixInhibit  	<- do.call(rbind, inputData$inhib)	  # matrix of the weights of inhibitory arcs, always going FROM places TO transitions
+inhibIndex 	<- which(matrixInhibit>=1, arr.ind=TRUE)  #see where are inhibitions in the matrix
+matrixInward 	<- do.call(rbind, inputData$post)       # matrix of edges going from transitions to places
+matrixOutward 	<- -1*(do.call(rbind, inputData$pre)) # matrix of edges going from places to transitions
 matrixDelta 	<- matrixInward + matrixOutward
 colnames(matrixInhibit) <- colnames(matrixInward) <- colnames(matrixOutward) <- colnames(matrixDelta) <- placesNames
 
@@ -49,65 +50,77 @@ colnames(matrixInhibit) <- colnames(matrixInward) <- colnames(matrixOutward) <- 
 
 dir.create ("Results", showWarnings = FALSE)
 
-Simulcore <- function() {  #the core of the simulation; each step is repeated for stepsNumber times (x=1 is the starting condition)
-	matrixMatrix 	<- matrix(ncol =kplaceN+1, nrow= stepsNumber)
-	matrixMatrix[1,]<- c(matrixTokens,0)
-	totTime 	<- 0
-
-	for (matrixRow in 1:stepsNumber) {
-		vectorTrans <- 1:ktransitN				
-		tokenSum    <- rowSums(-1*sweep(matrixOutward,2,matrixTokens,`*`))
-		tokenSum[which(tokenSum == 0)] = 1
-		vectorProb  <- tokenSum*vectorPar
-		vectorProb  <- (-1/vectorProb)*log(runif(ktransitN,min=0, max=1))
-
-		repeat {
-			parTime	<- min(vectorProb)		
-			chance	<- which(vectorProb == parTime)
-			if (length(chance) >1)
-				chance	<- sample(chance,1)
-			rn	<- vectorTrans[chance]			
-
-			if (length(inhibIndex) == 0) {
-				if (all(matrixTokens >= -matrixOutward[rn,])) {
-					#the real transition is completed, adding tokens to postplaces and removing them from preplaces
-  				matrixTokens <- matrixTokens + matrixDelta[rn,]
-					totTime      <- totTime + parTime
-					matrixMatrix[matrixRow,] <- c(matrixTokens, totTime)
-	  			 	break
-				}
-			} else {
-				indexIndex <- inhibIndex[which(inhibIndex[,1] ==rn),2]	#see whether and where are inhibitions arcs involved in that transition
-				if ((length(indexIndex) == 0) || (all(matrixTokens[indexIndex] < matrixInhibit[rn,indexIndex]))) {
-					if (all(matrixTokens >= -matrixOutward[rn,])) {
-						#the real transition is completed, adding tokens to postplaces and removing them from preplaces
-  					matrixTokens <- matrixTokens + matrixDelta[rn,]
-						totTime	     <- totTime + parTime
-						matrixMatrix[matrixRow,] <- c(matrixTokens, totTime)
-	 	 			 	break
-					}
-				}
-			}
-
-			# the picked transition could be disabled because there is inhibition or because the tokens you have are less than those you need;
-			vectorTrans <- vectorTrans[!vectorTrans == rn]	# if that, you repeat the picking until you pick a good one!
-			vectorProb  <- vectorProb[-chance]
-			if 	(length(vectorTrans) == 0){
-				# if you pick all transitions and they are all disabled, you've surely reached a dead state; therefore the whole simulation stops
- 				.Internal(cat(list("\t	dead state!!"), stdout(), " ", FALSE, NULL, FALSE))
-				matrixMatrix[matrixRow,] <- c(matrixTokens, -totTime)
-				return(matrixMatrix[c(1:matrixRow),])
-  			}
-		}
-		
-	}
-	return(matrixMatrix[c(1:matrixRow),])
+Simulcore <- function() {  # the core of the simulation; it is employed later in the script
+  # variables initialized
+  matrixMatrix 	<- matrix(ncol =kplaceN+1, nrow= stepsNumber)
+  matrixMatrix[1,]<- c(matrixTokens,0)
+  totTime 	<- 0
+  
+  # each step is repeated for stepsNumber times (x=1 is the starting condition)
+  for (matrixRow in 1:stepsNumber) {
+    # the following lines implement the core of the Gillespie algorithm;
+    vectorTrans <- 1:ktransitN				                                  # a probability vector is initialized
+    tokenSum    <- rowSums(-1*sweep(matrixOutward,2,matrixTokens,`*`))  # the nr of tokens in all the input places are assigned to each transition
+    tokenSum[which(tokenSum == 0)] = 1                                  # source transitions would have 0 tokens; here, 1 token is assigned by default   
+    vectorProb  <- tokenSum*vectorPar                                   # probability vector completed
+    vectorTime  <- 0
+    for (x in 1:ktransitN) {                                            
+      # random time is assigned to each transition (according an the exponential distribution with rate=probability)
+      if (vectorProb[x] <= 0) {vectorTime[x] = Inf}
+      else {vectorTime[x] <-rexp(1,vectorProb[x])}
+    }
+    
+    repeat {
+      # the shortest transition is chosen and its time recorded
+      parTime	<- min(vectorTime)		
+      chance	<- which(vectorTime == parTime)
+      if (length(chance) >1)
+        chance	<- sample(chance,1)
+      rn	<- vectorTrans[chance]			
+      
+      if (length(inhibIndex) == 0) {
+        # option 1: there is no inibitory edge in the network
+        if (all(matrixTokens >= -matrixOutward[rn,])) {
+          # if the transition is enabled, then it fires adding tokens to postplaces and removing them from preplaces
+          matrixTokens <- matrixTokens + matrixDelta[rn,]       # matrixTokens are updated according to the transition chosen
+          totTime      <- totTime + parTime                     # total time is updated by adding the duration of the firing transition
+          matrixMatrix[matrixRow,] <- c(matrixTokens, totTime)  # matrixTokens and the total elapsed time are recorded in the general matrix
+          break
+        }
+      } else {
+        # option 2: there are some inibitory edges in the network
+        indexIndex <- inhibIndex[which(inhibIndex[,1] ==rn),2]	#see whether and where are inhibitions arcs involved in that transition
+        if ((length(indexIndex) == 0) || (all(matrixTokens[indexIndex] < matrixInhibit[rn,indexIndex]))) {
+          if (all(matrixTokens >= -matrixOutward[rn,])) {
+            # if the transition is enabled, then it fires , adding tokens to postplaces and removing them from preplaces
+            matrixTokens <- matrixTokens + matrixDelta[rn,]      # matrixTokens are updated according to the transition chosen
+            totTime	     <- totTime + parTime                    # total time is updated by adding the duration of the firing transition
+            matrixMatrix[matrixRow,] <- c(matrixTokens, totTime) # matrixTokens and the total elapsed time are recorded in the general matrix
+            break
+          }
+        }
+      }
+      
+      # if you have reached this point that means that the picked transition was disabled (inhibition or lack of the required number of tokens in the input places);
+      vectorTrans <- vectorTrans[!vectorTrans == rn]	# if that, you repeat the picking until you pick a good one!
+      vectorTime  <- vectorTime[-chance]
+      if 	(length(vectorTrans) == 0){
+        # if you have picked all transitions and they were all disabled, you've surely reached a dead state; therefore the whole simulation stops
+        .Internal(cat(list("\t	dead state!!"), stdout(), " ", FALSE, NULL, FALSE))
+        matrixMatrix[matrixRow,] <- c(matrixTokens, -totTime)  # the last computed matrixTokens is recorded in the matrix together with a negative time (flagging dead states!)
+        return(matrixMatrix[c(1:matrixRow),])                  
+      }
+    }
+    
+  }
+  return(matrixMatrix[c(1:matrixRow),])
 }
 
 for (iterCounter in 1:iterNumber) {
-  #the entire simulation is repeated "iterNumber" number of times
+  # the entire simulation is repeated "iterNumber" number of times
   tempOutput	<- Simulcore()
   outputFile3  <- paste (outputPath, "simulation", iterCounter,".txt", sep = "")
+  # each simulation produces one file containing the general matrix
   if (is.matrix(tempOutput)) {
     write.table  (tempOutput, outputFile3, quote = FALSE, sep = "\t", row.names = FALSE, col.names = c(placesNames,"Time"))
   } else {write.table  (t(tempOutput), outputFile3, quote = FALSE, sep = "\t", row.names = FALSE, col.names = c(placesNames,"Time"))}
